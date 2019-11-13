1e4c246b163cf8859e3d553e7db582919b409840
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-13 18:14:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,8 +27,8 @@ class Student
     SQL
 
     DB[:conn].execute(sql, name).map(){|row|
-      self.new_from_db(row).
-    }
+      self.new_from_db(row)
+    }.first
     # find the student in the database given a name
     # return a new instance of the Student class
   end
