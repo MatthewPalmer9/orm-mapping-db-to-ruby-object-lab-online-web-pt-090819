33b0217e048d87ab7145308465fa8961b9ccb94a
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-13 18:16:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,14 @@ class Student
     # return a new instance of the Student class
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 9
+    SQL
+
+    DB[:conn]
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
