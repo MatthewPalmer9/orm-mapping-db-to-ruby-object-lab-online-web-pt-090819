c8d945b2b9442903d2afa262c420141a5b5d27ad
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-13 19:36:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,11 @@ class Student
 
   def self.first_X_students_in_grade_10
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 10
+    SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
+
+    DB[:conn].execute(sql)
+    binding.pry
   end
 
   def save
